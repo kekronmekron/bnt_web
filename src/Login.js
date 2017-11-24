7f27a94fe8bf6d08b126dc671b276383c0a453ea
badrimurali <badriitindia@gmail.com>
2017-11-24 19:09:48 +0530

    google sign in. include gapi as global. include google scripts to render custom button and include client id in index.html

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,31 @@
+/* global gapi */
 import React, {Component} from "react";
 
 export default class Login extends Component {
 
+  componentDidMount() {
+    this.renderGapi();
+  }
+
+  renderGapi() {
+    gapi.signin2.render("g-signin2", {
+      scope: "profile email",
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: "dark",
+      onsuccess: this.onSignIn.bind(this)
+    });
+  }
+  onSignIn(googleUser) {
+    const idToken = googleUser.getAuthResponse().id_token;
+  }
   render() {
     return (
       <div>
-        <h2>Login screen</h2>
+        <div style={{textAlign: "center", paddingTop: 200}}>
+          <div style={{display: "inline-block"}} id="g-signin2"></div>
+        </div>
       </div>
     );
   }
