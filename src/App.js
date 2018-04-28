0d32e5124ba6820c2136b04951c0e42f47e93f77
badrimurali <badriitindia@gmail.com>
2018-04-28 11:40:17 +0530

    change message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>BlockNTackle comin soon!!</h1>
+        <h1>BlockNTackle comin soon!! Be Patient!!</h1>
         <Login/>
       </div>
     );
